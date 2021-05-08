8af95efb30462e64586198134fcacf6bbaae6f52
StefanLetnikov <83037862+StefanLetnikov@users.noreply.github.com>
2021-05-08 20:53:13 +0200

    Add files via upload

---
+++ Veterinarna Ambulanta Milenici/Veterinarna Ambulanta Milenici/NovTermin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinarna_Ambulanta_Milenici
+{
+    public class NovTermin
+    {
+        public string ImeNaPacient { get; set; }
+        public string DateAndTime { get; set; }
+        public string Opis { get; set; }
+
+        public NovTermin() { }
+
+        public NovTermin(string ImeNaPacient,string DateAndTime,string Opis)
+        {
+            this.ImeNaPacient = ImeNaPacient;
+            this.DateAndTime = DateAndTime;
+            this.Opis = Opis;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Име на пациент:{0}, Датум и час на преглед:{1}\nОпис за преглед:{2}",ImeNaPacient,DateAndTime,Opis);
+        }
+    }
+}
---
+++ Veterinarna Ambulanta Milenici/Veterinarna Ambulanta Milenici/Termin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veterinarna_Ambulanta_Milenici
+{
+    public partial class Termin : Form
+    {
+        public Termin()
+        {
+            InitializeComponent();
+        }
+
+        private void Termin_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnVnesi_Click(object sender, EventArgs e)
+        {
+            NovTermin novTermin = new NovTermin();
+            novTermin.ImeNaPacient = tbTerminImePacient.Text;
+            novTermin.DateAndTime = dtpDatumICas.Text;
+            novTermin.Opis = rtbKratokOpis.Text;
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnOtkazi_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
